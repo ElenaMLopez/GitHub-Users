7bcbc3ea9ccfa2d9eb2dbb12deb4fb76b269d3cf
Elena Mateos Lopez <elena.mateos1@gmail.com>
2017-06-03 18:21:53 +0200

    added archives and forlders, basics html, css and js variables declarations

---
+++ AJAX/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var getUserName = document.getElementById('user-get');
+var search = document.getElementById('searchButton');
+var userName = document.getElementById('user-name');
+// falta foto
+var repositories = getElementById('repositories-number');
