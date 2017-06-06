e6725da1be9d3408d0277ce847a50a2a58e8a808
Elena Mateos Lopez <elena.mateos1@gmail.com>
2017-06-06 20:40:23 +0200

    final version

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,30 +1,37 @@
 'use strict';
 
 var getUserName = document.getElementById('user-get');
-var search = document.getElementById('searchButton');
+var searchButton = document.getElementById('searchButton');
 var userName = document.getElementById('user-name');
-// falta foto
+var picture= document.getElementById('pic');
 var repositories = document.getElementById('repositories-number');
-// creación de variable para obtener la respuesta
 
-var request = new XMLHttpRequest(); // XML tipo de request, http protocolo
+function askForUser() {
 
+  var request = new XMLHttpRequest(); // XML tipo de request, http protocolo
 
-request.open('GET', 'https://api.github.com/users/guerrero', true); // insertar variable usuario
+  request.open('GET', 'https://api.github.com/users/' + getUserName.value, true); // insertar variable usuario
 
-// Crear un if para abarcar las posibles ocurrencias:
-if (request.status >= 200 && request.status < 400) {
-  var objetJsFromJason = JSON.parse(request.responseText); //pasa la info JSON a
-  //js y lo guarda como valor de la propiedad responseText
-  // FALTAN poner la insercion en HTMl antes visualizar el JSON
-  console.log(request.responseText);
-}else {
-  console.log('Eror 404, el archivo no se encuentra');
+  request.onload = function(){
+
+    if (request.status >= 200 && request.status < 400) {
+      var objetJsFromJason = JSON.parse(request.responseText);
+
+      picture.style.display = "inline";
+
+      userName.innerHTML = objetJsFromJason.name;
+      picture.src = objetJsFromJason.avatar_url;
+      repositories.innerHTML = objetJsFromJason.public_repos;
+    }else {
+      console.log('Eror 404, el archivo no se encuentra');
+    }
+
+  };
+  request.onerror = function() {
+    alert('Error al tratar de conectarse con el servidor');
+  };
+
+  request.send();
+
 }
-
-request.onerror = function() {
-  console.log('Error al tratar de conectarse con el servidor');
-};
-// Una vez determinadas todas las opciones, hacemos la petición
-request.send();
-console.log(request.responseText);
+searchButton.addEventListener('click', askForUser);
